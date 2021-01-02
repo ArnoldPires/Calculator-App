cb9fdee340df501b8921e1227e08f3572b3fdd19
ArnoldPires <arnold.pires92@gmail.com>
2021-01-02 17:43:07 -0500

    moving around files/removed old code

--- js/main.js
+++ js/main.js
@@ -1,22 +1,34 @@
-function run(num)
-{
-document.cal.result.value +=num;
+let memory;
+let val;
+function displayVal() {
+  document.getElementById("display").value = val;
 }
-
-function runplus()
-{
-document.cal.result.value += "+";
+function memRemember(val) {
+	// display value from memory
+	val = memory;
+	document.getElementById("display").value = val;
 }
-function runmult()
-{
-document.cal.result.value += "*";
+function replace(val) {
+	// replace value in the display window with 'val' argument
+	document.getElementById("display").value = val;
 }
-function rundiv()
-{
-document.cal.result.value += "/";
+function append(val) {
+	// append new value onto existing display
+	document.getElementById("display").value += val;
 }
-function evalu()
-{
-var evalo = eval(document.cal.result.value)
-document.cal.result.value = evalo;
+function memPlus(val) {
+	document.getElementById("display").value = parseFloat(memory) + parseFloat(val);
+}
+function memSave(val) {
+	memory = document.getElementById("display").value;
+}
+function equals() {
+	try{ 		// test the statement to catch errors
+		replace(
+			eval(document.getElementById("display").value)  // solve the math expression
+		)
+	}
+	catch(err) {
+		replace('Error') // display custom text if error occurs
+	}
 }
